bb481f1957217a849d50ecca9b3d4221fd4a02df
malken21 <Malken_Marumasa@outlook.jp>
2022-02-11 21:07:19 +0900

    Delete gun1 directory

--- gun1/script.js
+++
@@ -1,9 +0,0 @@
-function audio(){
-    var audio = new Audio();
-    audio.src = "gun1.mp3";
-    audio.play();
-}
-function OnButtonClick(){
-    console.log("test");
-    document.body.requestFullscreen();
-}
