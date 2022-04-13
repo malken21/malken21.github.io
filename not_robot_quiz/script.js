8793ec88b2a7dc525be3e48ed8b534313460f40c
malken21 <Malken_Marumasa@outlook.jp>
2022-04-14 00:00:29 +0900

    Update script.js

--- not_robot_quiz/script.js
+++ not_robot_quiz/script.js
@@ -8,7 +8,7 @@ function count() {
     document.getElementById("count").innerHTML = parameter;
 }
 
-let Reload = function () {
+let reload = function () {
     setTimeout(function () {
         window.location.href = `./?count=${parameter + 1}`;
     }, 1000);
