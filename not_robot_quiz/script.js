6f3aaa08cb4d04ad4d1bff4ca0a75d0ee4c9f013
malken21 <Malken_Marumasa@outlook.jp>
2022-04-13 20:33:03 +0900

    Update script.js

--- not_robot_quiz/script.js
+++ not_robot_quiz/script.js
@@ -1,12 +1,3 @@
-var onloadCallback = function () {
-    grecaptcha.render('recaptcha', {
-        'sitekey': "6LeTtYgdAAAAANgraANZ3KqodcCgWmONpdV4xsil",
-        'callback': myAlert //コールバック関数名
-    });
-    var myAlert = function (response) {
-        var fn = function () {
-            console.log("1秒経過しました");
-        };
-        setTimeout(fn, 1000);
-    };
+var fn = function() {
+  console.log("10秒経過しました");
 };
