705c0578b0a802d1785cddc9bfdf81ed05a8dc8c
malken21 <Malken_Marumasa@outlook.jp>
2022-04-13 21:03:19 +0900

    Add files via upload

---
+++ not_robot_quiz/script.js
@@ -0,0 +1,10 @@
+var myAlert = function (response) {
+    var fn = function () {
+
+        let url = new URL(window.location.href);
+        let params = url.searchParams;
+
+        window.location.href = `../?count=${params.get('count')+1}`;
+    };
+    setTimeout(fn, 1000);
+};
