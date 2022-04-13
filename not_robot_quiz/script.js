a91567337fbc569616f2769d099d00b7436ad95b
malken21 <Malken_Marumasa@outlook.jp>
2022-04-13 20:26:31 +0900

    Update script.js

--- not_robot_quiz/script.js
+++ not_robot_quiz/script.js
@@ -1,3 +1,12 @@
-function recaptcha(){
-  window.location.href = 'https://malken21.github.io/not_robot_quiz/';
-}
+var onloadCallback = function () {
+    grecaptcha.render('recaptcha', {
+        'sitekey': "6LeTtYgdAAAAANgraANZ3KqodcCgWmONpdV4xsil",
+        'callback': myAlert //コールバック関数名
+    });
+    var myAlert = function (response) {
+        var fn = function () {
+            console.log("1秒経過しました");
+        };
+        setTimeout(fn, 1000);
+    };
+};
